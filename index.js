4ce7eb23519e4b9ddf2a507cded69b54250e6e96
Shane Vitarana <shanev@gmail.com>
2018-02-16 18:23:23 -0500

    create wallet with 2 accounts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bip39 = require('bip39');
 const Web3 = require('web3');
 
 const web3 = new Web3('https://ropsten.infura.io/6hHxPMt2sgN2OT6k0gLG');
+console.log(web3.version);
 
 var mnemonic = bip39.generateMnemonic();
 console.log(mnemonic);
@@ -19,3 +20,22 @@ console.log(account);
 //   signTransaction: [Function: signTransaction],
 //   sign: [Function: sign],
 //   encrypt: [Function: encrypt] }
+
+// web3.eth.getAccounts()
+// .then(console.log);
+// // []
+
+// web3.eth.getBlockNumber()
+// .then(console.log);
+// // 2666403
+
+// web3.eth.getBalance("0x407d73d8a49eeb85d32cf465507dd71d507100c1")
+// .then(console.log);
+
+// web3.eth.getBalance(account.address)
+// .then(console.log);
+
+web3.eth.accounts.wallet.create(2);
+
+const wallet = web3.eth.accounts.wallet;
+console.log(wallet);
