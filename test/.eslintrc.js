98004115d24c9012b499094292fba0ed8c864490
Shane Vitarana <shanev@gmail.com>
2018-02-22 15:41:18 -0500

    Added test folder

---
+++ test/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "rules": {
+  	"no-console": 0,
+    "import/no-extraneous-dependencies": [2, { devDependencies: true }],
+  }
+};
---
+++ test/api-test.js
@@ -0,0 +1,25 @@
+// const app = require('../app.js');
+// const jwt = require('jsonwebtoken');
+// const request = require('supertest').agent(app.listen());
+
+// describe('API', () => {
+
+//   describe('Users', () => {
+
+//     before(async () => {
+//     });
+
+//     describe('/api/v1/users/self/contacts', () => {
+//       describe('POST', () => {
+//         it('should return 200', (done) => {
+//           const data = { contacts: ['+18478338353', '+19089311417'] };
+//           request
+//             .post('/api/v1/users/self/contacts')
+//             .set('Authorization', `Bearer ${token}`)
+//             .send(data)
+//             .expect(200, done);
+//         });
+//       });
+//     });
+//   });
+// });
