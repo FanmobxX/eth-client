15820645b76e8f785f84f6047194b063040790b6
Shane Vitarana <shanev@gmail.com>
2018-02-22 14:48:37 -0500

    Added API router

--- api.js
+++ api.js
@@ -1,22 +1,37 @@
+const Router = require('koa-router');
+
+const router = new Router({
+  prefix: '/api/v1',
+});
 
 /**
  * Create and deploy artist token contract.
- * 
- * POST /artist/{artistId}/token/create
+ *
+ * POST /artists/{artistId}/tokens/create
  *
  * @param {string} artistId - The id of the artist in the db.
  * @param {string} tokenName - The name of the token, i.e: Tiga Coin.
  * @param {string} symbol - The symbol of the token, i.e: TIGA.
  */
+router.post('/artists/:id/token/create', async (ctx) => {
+  // const artistId = ctx.params.id;
+  ctx.status = 200;
+});
 
 /**
  * Create HD Wallet.
  *
  * POST /wallet/create
  */
+router.post('/users/:id/wallet/create', async (ctx) => {
+  ctx.status = 200;
+});
 
 /**
  * Create fandrop
  *
  * POST /artist/{artistId}/fandrop/create
  */
+router.post('/artists/:id/fandrop/create', async (ctx) => {
+  ctx.status = 200;
+});
--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(async (ctx, next) => {
   const start = new Date();
   await next();
   const ms = new Date() - start;
-  console.loog(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 // API - authentication required
