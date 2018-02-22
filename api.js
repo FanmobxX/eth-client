b5563dfa6bf6882754f6a6fc2ee02cfe528e9239
Shane Vitarana <shanev@gmail.com>
2018-02-22 14:06:42 -0500

    Added initial API design

---
+++ api.js
@@ -0,0 +1,22 @@
+
+/**
+ * Create and deploy artist token contract.
+ * 
+ * POST /artist/{artistId}/token/create
+ *
+ * @param {string} artistId - The id of the artist in the db.
+ * @param {string} tokenName - The name of the token, i.e: Tiga Coin.
+ * @param {string} symbol - The symbol of the token, i.e: TIGA.
+ */
+
+/**
+ * Create HD Wallet.
+ *
+ * POST /wallet/create
+ */
+
+/**
+ * Create fandrop
+ *
+ * POST /artist/{artistId}/fandrop/create
+ */
