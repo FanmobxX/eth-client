677ad4a2746a67daea4295447c9a737cb0060365
Shane Vitarana <shanev@gmail.com>
2018-02-28 16:55:39 -0500

    Changed JS functions to class

--- src/artist.js
+++ src/artist.js
@@ -8,140 +8,146 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETHEREUM_HTTP_PROVIDER));
 console.log(web3.version);
 
-function artistContract(contractName, tokenName, tokenSymbol) {
-  return `
-    pragma solidity ^0.4.18;
-
-    import './StandardToken.sol';
-
-    contract ${contractName} is StandardToken {
-
-      string public constant name = "${tokenName}";
-      string public constant symbol = "${tokenSymbol}";
-      uint8 public constant decimals = 18;
-
-      uint256 public constant INITIAL_SUPPLY = 10000000 * (10 ** uint256(decimals));
-
-      function ${contractName}() public {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
-        Transfer(0x0, msg.sender, INITIAL_SUPPLY);
-      }
+class ArtistContract {
+  constructor(contractName, tokenName, tokenSymbol) {
+    if ((contractName == null) || (tokenName == null) || (tokenSymbol == null)) {
+      throw new Error('A required parameter is missing');
     }
-  `;
-}
 
-async function estimateGas(contract, bytecode) {
-  let estimatedGas;
+    const source = ArtistContract.template(contractName, tokenName, tokenSymbol);
+    this.compiledContract = ArtistContract.compile(source);
+  }
 
-  await contract
-    .deploy({
-      data: `0x${bytecode}`,
-      // arguments: ['hello world'],
-    })
-    .estimateGas((err, gas) => {
-      estimatedGas = gas;
-    });
+  static template(contractName, tokenName, tokenSymbol) {
+    return `
+      pragma solidity ^0.4.18;
 
-  return estimatedGas;
-}
+      import './StandardToken.sol';
 
-/**
- * Get account balance
- * @return {string} Account balance in Ether
- */
-async function getBalance() {
-  const accounts = await web3.eth.getAccounts();
-  const balance = await web3.eth.getBalance(accounts[0]);
-  return web3.utils.fromWei(balance, 'ether');
-}
+      contract ${contractName} is StandardToken {
 
-/**
- * Compiles contract
- * @return {[type]} Compiled contract
- */
-function compile() {
-  const contractName = 'TigaToken';
-  const source = artistContract(contractName, 'Tiga Coin', 'TIGA');
+        string public constant name = "${tokenName}";
+        string public constant symbol = "${tokenSymbol}";
+        uint8 public constant decimals = 18;
 
-  const input = {
-    source,
-    'SafeMath.sol': fs.readFileSync('./contracts/SafeMath.sol').toString(),
-    'ERC20Basic.sol': fs.readFileSync('./contracts/ERC20Basic.sol').toString(),
-    'ERC20.sol': fs.readFileSync('./contracts/ERC20.sol').toString(),
-    'BasicToken.sol': fs.readFileSync('./contracts/BasicToken.sol').toString(),
-    'StandardToken.sol': fs.readFileSync('./contracts/StandardToken.sol').toString(),
-  };
+        uint256 public constant INITIAL_SUPPLY = 10000000 * (10 ** uint256(decimals));
 
-  // Compile contract
-  const output = solc.compile({ sources: input }, 1);
-  return output.contracts['source:TigaToken'];
-}
-
-// function compile() {
-//   const input = {
-//     'Greeter.sol': fs.readFileSync('./contracts/Greeter.sol').toString(),
-//   };
-
-//   // Compile contract
-//   const output = solc.compile({ sources: input }, 1);
-//   const c = output.contracts['Greeter.sol:greeter'];
-//   // console.log(output.contracts);
-//   return c;
-// }
-
-/**
- * Deploys contract
- * @param {contract} compiledContract The solc compiled contract
- * @return {string} Contract address
- */
-async function deploy(compiledContract) {
-  const { bytecode } = compiledContract;
-  const abi = JSON.parse(compiledContract.interface);
-  const contract = new web3.eth.Contract(abi);
-  const accounts = await web3.eth.getAccounts();
-  console.log(accounts);
-
-  // Deploy contract
-  await contract
-    .deploy({
-      data: web3.utils.toHex(bytecode),
-      // arguments: ['hello world'],
-    })
-    .send({
-      from: accounts[0],
-      gas: web3.utils.toHex(1500000),
-      gasPrice: web3.utils.toHex(web3.utils.toWei('20', 'gwei')),
-    }, (error, transactionHash) => {
-      if (error) {
-        console.log(error);
-        console.error(error);
+        function ${contractName}() public {
+          totalSupply_ = INITIAL_SUPPLY;
+          balances[msg.sender] = INITIAL_SUPPLY;
+          Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+        }
       }
-      console.log(transactionHash);
-    })
-    .on('error', (error) => {
-      console.log(error);
-    })
-    .on('transactionHash', (transactionHash) => {
-      console.log(transactionHash);
-    })
-    .on('receipt', (receipt) => {
-      // contains the new contract address
-      console.log(receipt.contractAddress);
-    })
-    .on('confirmation', (confirmationNumber, receipt) => {
-      console.log(receipt);
-    })
-    .then((newContractInstance) => {
-      console.log(newContractInstance.options.address);
-    });
+    `;
+  }
 
-  // return contractInstance.options.address;
+  // static compile() {
+  //   const input = {
+  //     'Greeter.sol': fs.readFileSync('./contracts/Greeter.sol').toString(),
+  //   };
+
+  //   // Compile contract
+  //   const output = solc.compile({ sources: input }, 1);
+  //   const c = output.contracts['Greeter.sol:greeter'];
+  //   return c;
+  // }
+
+  /**
+   * Compiles contract
+   * @return {[type]} Compiled contract
+   */
+  static compile(source) {
+    const input = {
+      source,
+      'SafeMath.sol': fs.readFileSync('./contracts/SafeMath.sol').toString(),
+      'ERC20Basic.sol': fs.readFileSync('./contracts/ERC20Basic.sol').toString(),
+      'ERC20.sol': fs.readFileSync('./contracts/ERC20.sol').toString(),
+      'BasicToken.sol': fs.readFileSync('./contracts/BasicToken.sol').toString(),
+      'StandardToken.sol': fs.readFileSync('./contracts/StandardToken.sol').toString(),
+    };
+
+    // Compile contract
+    const output = solc.compile({ sources: input }, 1);
+    return output.contracts['source:TigaToken'];
+  }
+
+  /**
+   * Get account balance
+   * @return {string} Account balance in Ether
+   */
+  static async getBalance() {
+    const accounts = await web3.eth.getAccounts();
+    const balance = await web3.eth.getBalance(accounts[0]);
+    return web3.utils.fromWei(balance, 'ether');
+  }
+
+  /**
+   * Deploys contract
+   * @param {contract} compiledContract The solc compiled contract
+   * @return {string} Contract address
+   */
+  async deploy() {
+    const { bytecode } = this.compiledContract;
+    const abi = JSON.parse(this.compiledContract.interface);
+    const contract = new web3.eth.Contract(abi);
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+
+    // Deploy contract
+    contract
+      .deploy({
+        data: web3.utils.toHex(bytecode),
+        // arguments: ['hello world'],
+      })
+      .send({
+        from: accounts[0],
+        gas: web3.utils.toHex(1500000),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('20', 'gwei')),
+      }, (error, transactionHash) => {
+        if (error) {
+          console.log(error);
+          console.error(error);
+        }
+        console.log(transactionHash);
+      })
+      .on('error', (error) => {
+        console.log(error);
+      })
+      .on('transactionHash', (transactionHash) => {
+        console.log(transactionHash);
+      })
+      .on('receipt', (receipt) => {
+        // contains the new contract address
+        console.log(receipt.contractAddress);
+      })
+      .on('confirmation', (confirmationNumber, receipt) => {
+        console.log(receipt);
+      })
+      .then((newContractInstance) => {
+        console.log(newContractInstance.options.address);
+      });
+  }
+
+  /**
+  * Estimate gas.
+  * @param  {web3.eth.Contract} contract The web3 contract
+  * @return {string} The gas amount
+  */
+  async estimateGas() {
+    let estimatedGas;
+    const abi = JSON.parse(this.compiledContract.interface);
+    const contract = new web3.eth.Contract(abi);
+
+    await contract
+      .deploy({
+        data: `0x${this.compiledContract.bytecode}`,
+        // arguments: ['hello world'],
+      })
+      .estimateGas((err, gas) => {
+        estimatedGas = gas;
+      });
+    return estimatedGas;
+  }
 }
 
-module.exports = {
-  getBalance,
-  compile,
-  deploy,
-  estimateGas,
-};
+module.exports = ArtistContract;
--- test/artist-test.js
+++ test/artist-test.js
@@ -1,47 +1,35 @@
 const assert = require('assert');
-const {
-  compile,
-  estimateGas,
-  deploy,
-  getBalance,
-} = require('../src/artist.js');
+const ArtistContract = require('../src/artist.js');
 const Web3 = require('web3');
 
 // Connect to Ethereum
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETHEREUM_HTTP_PROVIDER));
 console.log(web3.version);
 
-describe('Artist', () => {
-  let compiledContract = null;
+describe('ArtistContract', () => {
+  const contractName = 'TigaToken';
+  const tokenName = 'Tiga Coin';
+  const tokenSymbol = 'TIGA';
+  const artistContract = new ArtistContract(contractName, tokenName, tokenSymbol);
 
   it('should check account balance', async () => {
-    const balance = await getBalance();
+    const balance = await ArtistContract.getBalance();
     console.log(`Current balance: ${balance} ETH`);
     assert(balance > 0);
   });
 
   it('should compile contract', (done) => {
-    compiledContract = compile();
-    assert(compiledContract);
+    assert(artistContract.compiledContract);
     done();
   }).timeout(20000);
 
   it('should estimate gas', async () => {
-    const { bytecode } = compiledContract;
-    const abi = JSON.parse(compiledContract.interface);
-
-    // Contract object
-    const contract = new web3.eth.Contract(abi);
-
-    const gas = await estimateGas(contract, bytecode);
+    const gas = await artistContract.estimateGas();
     console.log(`Estimated gas: ${gas}`);
     assert(gas > 0);
   });
 
   it('should deploy artist contract', async () => {
-    // deploy(compiledContract, done);
-    await deploy(compiledContract);
-    // console.log(contractAddress);
-    // assert.equal(contractAddress.length, 42);
-  }).timeout(200000);
+    await artistContract.deploy();
+  }).timeout(20000);
 });
