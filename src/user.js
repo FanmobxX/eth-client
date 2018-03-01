42447f1a04557cb8ad05e4ac5e525c057bbc894d
Shane Vitarana <shanev@gmail.com>
2018-03-01 16:45:13 -0500

    class-ify

--- src/user.js
+++ src/user.js
@@ -1,23 +1,22 @@
 require('dotenv').config();
 
-// const fs = require('fs');
-// const solc = require('solc');
 const Web3 = require('web3');
 
 // Connect to Ethereum
 const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETHEREUM_HTTP_PROVIDER));
-console.log(web3.version);
 
-const account = web3.eth.accounts.create();
-// console.log(account);
-const { privateKey } = account;
-// const recreatedAccount = web3.eth.accounts.privateKeyToAccount(privateKey);
-// console.log(recreatedAccount);
+class Account {
+  static create() { // id
+    const account = web3.eth.accounts.create();
+    const { privateKey } = account;
+    const keystore = web3.eth.accounts.encrypt(privateKey, process.env.KEYSTORE_PW);
+    console.log(keystore); // save in mongodb
+  }
 
-const password = 'password';
+  // static find(id) {
+  //   const decrypted = web3.eth.accounts.decrypt(keystore, process.env.KEYSTORE_PW);
+  //   console.log(decrypted);
+  // }
+}
 
-const keystore = web3.eth.accounts.encrypt(privateKey, password);
-console.log(keystore);
-
-const decrypted = web3.eth.accounts.decrypt(keystore, password);
-console.log(decrypted);
+module.exports = Account;
