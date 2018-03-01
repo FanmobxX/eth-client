ca5ec6d65a6b1ec405d07edd8f6a22ef42738430
Shane Vitarana <shanev@gmail.com>
2018-03-01 14:04:42 -0500

    configured truffle for testnet

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
 module.exports = {
   networks: {
-    development: {
+    local: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*',
+    },
+    test: {
+      host: '198.199.66.129',
+      port: 8545,
+      network_id: '*',
+      gas: 6431652,
     },
   },
 };
